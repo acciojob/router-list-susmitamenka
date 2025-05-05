53d7c3dd668db96f6898ed5705ac4c7cac9eca7c
susmita <susmitaprasad73@gmail.com>
2025-05-05 22:55:15 +0530

    hii

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ function Home(){
         <>
         <h1>Item List</h1>
         <ul>
-            <li><Link to="/Item1">Item1</Link></li>
-            <li><Link to="/Item2">Item2</Link></li>
-            <li><Link to="/item3">Item3</Link></li>
+            <li><Link to="/Item1">Item 1</Link></li>
+            <li><Link to="/Item2">Item 2</Link></li>
+            <li><Link to="/item3">Item 3</Link></li>
         </ul>
         </>
     )
